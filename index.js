757870c51c4724eba0bb56d51cb9fba64e919bd8
Leonardo Marziali <leonardo.marziali@accenture.com>
2024-04-12 16:16:15 +0200

    Add square function

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// This is a simple Node.js script
+// This is an updated version of the index.js file
 
 // Function to greet the user
 function greet(name) {
     console.log(`Hello, ${name}!`);
 }
 
+// Function to calculate the square of a number
+function square(num) {
+    return num * num;
+}
+
 // Call the greet function
 greet("World");
+
+// Call the square function
+console.log("The square of 5 is:", square(5));
