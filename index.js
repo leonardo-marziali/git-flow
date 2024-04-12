61107affeb72eb0cec7467c06bb6b597c04c9e62
Leonardo Marziali <leonardo.marziali@accenture.com>
2024-04-12 16:17:47 +0200

    Add cube function

--- index.js
+++ index.js
@@ -1,9 +1,17 @@
-// This is a simple Node.js script
+// This is an updated version of the index.js file
 
 // Function to greet the user
 function greet(name) {
     console.log(`Hello, ${name}!`);
 }
 
+// Function to calculate the cube of a number
+function cube(num) {
+    return num * num * num;
+}
+
 // Call the greet function
 greet("World");
+
+// Call the cube function
+console.log("The cube of 3 is:", cube(3));
